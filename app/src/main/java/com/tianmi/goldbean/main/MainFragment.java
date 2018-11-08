08782ba206293539f53138ad65181eb6f030ffb4
nokiafc <nokiafc@126.com>
2018-11-08 20:41:41 +0800

    测试提交是否成功

--- app/src/main/java/com/tianmi/goldbean/main/MainFragment.java
+++ app/src/main/java/com/tianmi/goldbean/main/MainFragment.java
@@ -3,6 +3,7 @@ package com.tianmi.goldbean.main;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,14 @@ import android.view.ViewGroup;
 import com.tianmi.goldbean.R;
 
 public class MainFragment extends Fragment {
-    @Nullable
+    private ViewPager viewPager;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        init(view);
         return view;
+    }
+    private void init(View view){
+        viewPager = (ViewPager)view.findViewById(R.id.viewPager);
     }
 }
