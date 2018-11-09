02a549ff9e642c0eaec9501ba62b28e3f8756acd
nokiafc <nokiafc@126.com>
2018-11-09 17:07:17 +0800

    首页viewpager带导航

---
+++ app/src/main/java/com/tianmi/goldbean/adapter/MainPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.tianmi.goldbean.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.tianmi.goldbean.R;
+import com.tianmi.goldbean.bean.PagerBean;
+
+import java.util.List;
+
+public class MainPagerAdapter extends PagerAdapter {
+    private List<PagerBean> list;
+    private Context context;
+    public MainPagerAdapter(List<PagerBean> list, Context context){
+        this.list = list;
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_view_pager, null);
+        ImageView imageView =  (ImageView) view.findViewById(R.id.img_view_pager);
+        Glide.with(context).load(R.drawable.pager).into(imageView);
+        container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+//        super.destroyItem(container, position, object);
+        container.removeView((View)object);
+    }
+}
--- app/src/main/java/com/tianmi/goldbean/main/MainFragment.java
+++ app/src/main/java/com/tianmi/goldbean/main/MainFragment.java
@@ -7,19 +7,83 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.tianmi.goldbean.R;
+import com.tianmi.goldbean.adapter.MainPagerAdapter;
+import com.tianmi.goldbean.bean.PagerBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment extends Fragment {
     private ViewPager viewPager;
+    private List<PagerBean> list = new ArrayList<PagerBean>();
+    private MainPagerAdapter mainPagerAdapter;
+    private LinearLayout dotLayout;
+
+
+    private ImageView[] imageViews;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         init(view);
-        //测试第二部
         return view;
     }
     private void init(View view){
+        for(int i=0; i<3;i++){
+            PagerBean bean = new PagerBean();
+            list.add(bean);
+        }
+
+        imageViews = new ImageView[list.size()];
+        dotLayout = (LinearLayout)view.findViewById(R.id.layout_dot);
         viewPager = (ViewPager)view.findViewById(R.id.viewPager);
+        mainPagerAdapter = new MainPagerAdapter(list, getActivity());
+        viewPager.setAdapter(mainPagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i=0; i<list.size(); i++){
+                    imageViews[i].setImageDrawable(getResources().getDrawable(R.drawable.shape_dot_1));
+                    if(position == i){
+                        imageViews[i].setImageDrawable(getResources().getDrawable(R.drawable.shape_dot));
+                    }
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(10, 0,0,0);
+
+        for(int i=0; i<list.size(); i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageViews[i] = imageView;
+            if(i ==0){
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.shape_dot));
+
+            }else {
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.shape_dot_1));
+            }
+
+
+            dotLayout.addView(imageView, params);
+        }
+
+
     }
 }
