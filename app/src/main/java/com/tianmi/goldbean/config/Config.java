54007ea99e0965f3f038d90df91be25c1ac72da3
ZN <3244783916@qq.com>
2018-11-18 13:05:48 +0800

    修改config中服务器地址

--- app/src/main/java/com/tianmi/goldbean/config/Config.java
+++ app/src/main/java/com/tianmi/goldbean/config/Config.java
@@ -3,10 +3,10 @@ package com.tianmi.goldbean.config;
 public class Config {
     public static String BASE_URL = "";
     public static boolean IS_DEBUG = true;
-    public static String PICTURE_URL = "http://59.111.61.145/tianmi/user/upload";
+    public static String PICTURE_URL = "http://59.111.105.183/tianmi/user/upload";
     static {
         if (IS_DEBUG) {//开发
-            BASE_URL = "http://59.111.61.145/tianmi";
+            BASE_URL = "http://59.111.105.183/tianmi";
 
         } else {//线上
             BASE_URL = "http://www.tianmi.com";
